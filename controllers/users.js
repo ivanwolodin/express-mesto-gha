60448c4aff51ca12849dd4080555e9e1985204a8
Ivan Volodin <ivanwolodin@gmail.com>
2022-05-05 16:19:36 +0500

    Fix: additional brackets

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { DataBaseError } = require(('../errors/dataBaseError'));
-const { NotFoundError } = require(('../errors/notFoundError'));
+const { DataBaseError } = require('../errors/dataBaseError');
+const { NotFoundError } = require('../errors/notFoundError');
 
 const User = require('../models/user');
 const {
